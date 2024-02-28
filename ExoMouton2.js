d61fb42bd19ebc66c638973e607d36bc5d4fa4c4
LaureBonnevalle <laure.bonnevalle@orange>
2024-02-28 15:36:59 +0000

    J12 Mouton

---
+++ ExoMouton2.js
@@ -0,0 +1,60 @@
+function selectColor(event)
+{
+    let colorBox = event.target;
+    let color = colorBox.style.backgroundColor;
+
+    sessionStorage.setItem("selectedColor", color);
+}
+
+function getSelectedColor()
+{
+    if(sessionStorage.getItem("selectedColor"))
+    {
+        return sessionStorage.getItem("selectedColor");
+    }
+
+    return null;
+}
+
+function loadPalette(palette)
+{
+    let colors = palette;
+    
+    // le code de l'étape 1 se passe ici
+    
+    //## solution avec for()
+    let divs = document.querySelectorAll(`header > div`);
+    for(let i = 0; i < divs.length; i++){
+        let div = divs[i];
+        div.style.backgroundColor = colors[i];
+    }
+    
+    //## solution avec forEach()
+    // let divs = document.querySelector(`header > div`);
+    // divs.forEach(function(color, key){
+    //   let div = divs[key];
+    //   div.style.backgroundColor = color;
+    // });
+}
+
+
+window.addEventListener("DOMContentLoaded", function(){
+    loadPalette(["#22f6f3", "#3daf7e", "#ffffff", "#ec8236", "#a9a7ee", "#ecc606", "#f783f2", "#e89e80"]);
+
+    // le code de l'étape 2 se passe ici
+    let headerDivs = document.querySelectorAll('header > div');
+    for(let i = 0; i < headerDivs.length; i++){
+        let div = headerDivs[i];
+        div.addEventListener("click", selectColor);
+    }
+    /*
+    // Méthode 2
+    headerDivs.forEach(function(div){
+      div.addEventListener("click", selectColor);
+    });
+    */
+    // console.log(getSelectedColor());
+
+    // le code de l'étape 3 se passe ici
+
+});
